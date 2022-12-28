e369cddcc72449e331a8fffbc104875fb63fdcdf
touzaelhassan <touzaelhassan@gmail.com>
2022-12-28 02:32:26 +0100

    Creating the project structure and necesary folders

---
+++ src/main/java/com/application/SpringBootApiEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiEndpointsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiEndpointsApplication.class, args);
+    }
+
+}
