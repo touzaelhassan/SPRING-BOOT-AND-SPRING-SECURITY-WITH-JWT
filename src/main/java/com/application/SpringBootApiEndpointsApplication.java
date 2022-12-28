dbe4b049bb294048a31110dcf04bfa992ec8d9db
touzaelhassan <touzaelhassan@gmail.com>
2022-12-28 09:18:41 +0100

    Spring Configuration authentication and authorization

--- src/main/java/com/application/SpringBootApiEndpointsApplication.java
+++ src/main/java/com/application/SpringBootApiEndpointsApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -43,5 +45,8 @@ public class SpringBootApiEndpointsApplication {
         };
 
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
 
 }
--- src/main/java/com/application/services/implementations/UserServiceImplementation.java
+++ src/main/java/com/application/services/implementations/UserServiceImplementation.java
@@ -7,18 +7,35 @@ import com.application.repositories.UserRepository;
 import com.application.services.specifications.UserServiceSpecification;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service("userServiceBean")
 @RequiredArgsConstructor
 @Transactional
-public class UserServiceImplementation implements UserServiceSpecification {
+public class UserServiceImplementation implements UserServiceSpecification, UserDetailsService {
 
     private final UserRepository userRepositoryBean;
     private final RoleRepository roleRepositoryBean;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        User user = userRepositoryBean.findByUsername(username);
+        if(user == null) { throw new UsernameNotFoundException("User not found in the database !!."); }
+        user.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+
+    }
 
     @Override
     public User saveUser(User user) { return userRepositoryBean.save(user); }
