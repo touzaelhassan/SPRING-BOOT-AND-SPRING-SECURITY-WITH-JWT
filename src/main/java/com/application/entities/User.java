b9560d93ae7d12155564dbf5e0700e547e70eee7
touzaelhassan <touzaelhassan@gmail.com>
2022-12-28 03:27:14 +0100

    Creating User and Role repositories

--- src/main/java/com/application/entities/User.java
+++ src/main/java/com/application/entities/User.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
---
+++ src/main/java/com/application/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.application.repositories;
+
+import com.application.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+     User findByUsername(String username);
+
+}
